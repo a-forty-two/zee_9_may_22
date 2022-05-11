db5582498248abc143df67b7542dd21b998e8267
Shantanu Pandey <shantanu.pandey@live.in>
2022-05-11 11:07:37 +0530

    Create api.py

---
+++ api.py
@@ -0,0 +1,13 @@
+from flask import Flask
+
+app = Flask('myapp')
+
+@app.route('/')
+def blahblah():
+  return "<p> This works </p> <br/> <br/> <a href="wwww.pikachu.com"> Pika Pika </a> "
+
+@app.route('/sum/<a>/<b>')
+def mysum(a, b):
+  return str(int(a) + int(b))
+
+app.run(host="0.0.0.0")
